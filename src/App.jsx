45d59285f9d7d6a0cbb52046eb503daeefeda8bc
David Fernadez <daffevafernandez@gmail.com>
2024-07-17 12:29:22 +0200

    feat: function login created

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,13 @@ function App() {
     if (e.target.name === 'password') {
       setPassword(e.target.value)
     }
+  }
 
+  function login() {
+    console.log('Login')
+    if(email.length === 0 || password.length === 0){
+      return alert('Email and password are required')
+    }
     console.log(email)
     console.log(password)
   }
@@ -26,14 +32,14 @@ function App() {
       <h1>Login</h1>
       <div>
         {/* <label htmlFor="email">Email: </label> */}
-        <input type="text" name="email" placeholder='Email' onChange={handleChange} />
+        <input type="email" name="email" placeholder='Email' onChange={handleChange} />
       </div>
       <div>
         {/* <label htmlFor="password">Password: </label> */}
         <input type="password" name="password" placeholder='Password' onChange={handleChange} />
       </div>
       <div>
-        <input type="button" value="Login" />
+        <input type="button" value="Login" onClick={login} />
       </div>
     </>
   )
