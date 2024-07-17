9ee655dea2bcc24addf4ed4b239bd7418a42060a
David Fernadez <daffevafernandez@gmail.com>
2024-07-17 12:17:27 +0200

    feat: creation of handleChange for email and password

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  function handleChange(e) {
+    console.log('handleChange')
+
+    if (e.target.name === 'email') {
+      setEmail(e.target.value)
+    }
+
+    if (e.target.name === 'password') {
+      setPassword(e.target.value)
+    }
+
+    console.log(email)
+    console.log(password)
+  }
+
   return (
     <>
       <h1>Login</h1>
       <div>
         {/* <label htmlFor="email">Email: </label> */}
-        <input type="text" name="email" id="" placeholder='Email'/>
+        <input type="text" name="email" placeholder='Email' onChange={handleChange} />
       </div>
       <div>
         {/* <label htmlFor="password">Password: </label> */}
-        <input type="password" name="" id="" placeholder='Password'/>
+        <input type="password" name="password" placeholder='Password' onChange={handleChange} />
       </div>
       <div>
         <input type="button" value="Login" />
