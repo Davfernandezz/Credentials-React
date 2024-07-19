8606ced05bce335ca9fe75ab415569f8ce4a2c0d
David Fernadez <daffevafernandez@gmail.com>
2024-07-19 13:05:16 +0200

    feat: services includes in the view the name and description of the service

--- src/views/Services/Services.jsx
+++ src/views/Services/Services.jsx
@@ -1,21 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Services = () => {
-    useEffect(()=> {
-        console.log('UseEffect')
 
-        fetch('http://localhost:4000/api/services')
-        .then(res =>{
-            console.log(res);
-        })
-        .catch(e =>{
-            console.log(e);
-        })
-    })
+  const [services, setServices] = useState([])
+
+  useEffect(() => {
+    console.log('UseEffect')
+
+    fetch('http://localhost:4000/api/services')
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        setServices(res.data)
+        console.log(res);
+      })
+      .catch(e => {
+        console.log(e);
+      })
+  }, [])
 
   return (
     <>
-    <h1>Services</h1>
+      <h1>Services</h1>
+      {
+        services.map((service) => (
+          <div key={service.id} className='card'>
+          <h3>{service.service_name}</h3>
+          <p>{service.description}</p>
+          </div>
+        )
+        )
+      }
     </>
   )
 }
