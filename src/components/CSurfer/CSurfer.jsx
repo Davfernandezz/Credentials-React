d858cc20c8295971af7dce8c54abfdb9f8d58c2f
David Fernadez <daffevafernandez@gmail.com>
2024-07-24 18:09:08 +0200

    feat: header prototype completed

--- src/components/CSurfer/CSurfer.jsx
+++ src/components/CSurfer/CSurfer.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import './CSurfer.css';
 
-export const CSurfer = (
-    {
-        content,
-        path
-    }
-) => {
+export const CSurfer = ({
+    content,
+    path
+}) => {
     const navigate = useNavigate();
 
     return (
-        <div onClick={() => navigate(path)}>{content}</div>
+        <div className="csurfer" onClick={() => navigate(path)}>{content}</div>
     )
 }
