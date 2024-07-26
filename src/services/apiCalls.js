bb205dc266afbcbbd23d4363d7e42709a6c5c2d6
David Fernadez <daffevafernandez@gmail.com>
2024-07-26 10:20:30 +0200

    feat: implementation of try catch in apiCalls

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -2,70 +2,95 @@
 const URL = 'http://localhost:4000/api'
 
 export const registerUser = async (credentials) => {
-    const request = await fetch(`${URL}/register`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(credentials),
-    });
-    const result = await request.json();
-    return result;
+    try {
+        const request = await fetch(`${URL}/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(credentials),
+        });
+        const result = await request.json();
+        return result;
+    } catch (error) {
+
+    }
 }
 
+
 export const loginUser = async (credentials) => {
-    const request = await fetch(`${URL}/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(credentials),
-    });
-    const result = await request.json();
-    return result;
+    try {
+        const request = await fetch(`${URL}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(credentials),
+        });
+        const result = await request.json();
+        return result;
+    } catch (error) {
+
+    }
 }
 
 export const getUserProfile = async (token) => {
-    const response = await fetch(`${URL}/users/profile`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        }
-    });
-    return await response.json()
+    try {
+        const response = await fetch(`${URL}/users/profile`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        return await response.json()
+    } catch (error) {
+
+    }
 }
 
 export const updateProfile = async (changes, token) => {
-    const response = await fetch(`${URL}/profile/update`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        },
-        body: JSON.stringify(changes),
-    });
-    return await response.json()
+    try {
+        const response = await fetch(`${URL}/profile/update`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify(changes),
+        });
+        return await response.json()
+    } catch (error) {
+
+    }
 }
 
 export const getUsers = async (token) => {
-    const response = await fetch(`${URL}/users`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        }
-    });
-    return await response.json()
+    try {
+        const response = await fetch(`${URL}/users`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        return await response.json()
+    } catch (error) {
+
+    }
 }
 
 export const deleteUserById = async (token, id) => {
-	const response = await fetch(`${URL}/users/${+id}`, {
-		method: "DELETE",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": `Bearer ${token}`
-		}
-	})
-	return await response.json()
+    try {
+        const response = await fetch(`${URL}/users/${+id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        })
+        return await response.json()
+    } catch (error) {
+
+    }
 }
